50a46af3e31a60404de62d3ef92fe621a06dbd01
aishwarya.suradkar <aishsuradkar03@gmail.com>
2023-09-27 07:41:33 +0530

    String arrays

--- charArray/stringArray.cpp
+++ charArray/stringArray.cpp
@@ -69,27 +69,67 @@ int main(){
 
 
 //2/
-    string name= "lOVE BABBAR ";
-    string middle= "Kumar ";
-    string str= "to be question";
-    string str2 = "or not to be";
+    // string name= "lOVE BABBAR ";
+    // string middle= "Kumar ";
+    // string str= "to be question";
+    // string str2 = "or not to be";
 
 
 
-    name.insert(5, middle);
-    cout<< "Printing name:" << name << endl;
+    // name.insert(5, middle);
+    // cout<< "Printing name:" << name << endl;
 
-    str.insert(str.size(),1,':');
-    cout<< str << endl;
+    // str.insert(str.size(),1,':');
+    // cout<< str << endl;
 
-    str.insert(str.end(),3,'.');
-    cout<< str << endl;
+    // str.insert(str.end(),3,'.');
+    // cout<< str << endl;
 
-    str.insert(str.begin()+5, ',');
-    cout << str << endl;
+    // str.insert(str.begin()+5, ',');
+    // cout << str << endl;
 
-    str.insert(6, str2, 3 ,4); // at 6th index inser str2 from str2 extract  4 characters starting from 3rd index
-    cout<< str << endl;
+    // str.insert(6, str2, 3 ,4); // at 6th index inser str2 from str2 extract  4 characters starting from 3rd index
+    // cout<< str << endl;
+
+
+//3.
+    // string name = "Love";
+
+    // cout << name << endl;
+
+    // name.push_back('R');
+    // cout << name << endl;
+
+    // name.pop_back();
+    // name.pop_back();
+    // cout << name << endl;
+
+
+//3.FINDING A STRING IN A STRING
+    // string str1 = "yaar tera superstar desi kalakar";
+    // string str2 = "star";
+    // string str3= "Star";
+
+    // if(str1.find(str2) == string:: npos){
+    //     //not found
+    //     cout <<"Not found"<< endl;
+    // }
+    // else{
+    //     cout<< "Found" << endl;
+    // }
+
+
+    // if(str2.compare(str3) == 0 ){
+    //     cout << str2 << " is " << str3 << endl;
+    // }
+    // else{
+    //     cout << str2 << " is not " << str3 << endl;
+    // }
+    
+
+    string str1 = "This is a car, big daddy of all suvs";
+    cout << str1.substr(19,5) << endl;
+
 
 
 
