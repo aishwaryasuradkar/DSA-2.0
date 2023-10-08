ccc3e5573eb83e9d8c2f2a384697f49c0209edbd
aishwarya.suradkar <aishsuradkar03@gmail.com>
2023-10-08 19:40:57 +0530

    Pointers

---
+++ Pointers/pointers.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a = 5;
+    //cout << a << endl;
+    //cout << "Address of a: " << &a << endl;
+
+
+//1.CREATION
+    // int* ptr = &a;
+
+
+//2.Accessing value stored at 'ptr' which is the address of 'a' (this will print 'a')
+    // cout << "Accessing: " << *ptr << endl; // address vab ka vab
+
+    // cout << "a ka address: " << ptr << endl; //a ka address 
+
+    // cout<< "ptr dabbe ka address: "<< &ptr << endl; //ptr wale dabbe ka address
+
+
+//3.
+    // int a = 5;
+    // int* ptr = &a;
+    // cout<<sizeof(ptr) << endl;
+
+    // char ch = 'k';
+    // char* cptr = &ch;
+    // cout<< sizeof(cptr) << endl;
+
+
+    // long lachi =  10;
+    // long* lptr = &lachi;
+    // cout << sizeof(lptr) << endl;
+
+
+
+//4. ACCESSING BUT A BAD PRACTICE
+    int* ptr;
+    cout<< *ptr << endl; 
+
+
+
+
+
+
+
+    return 0;
+}
